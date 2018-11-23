c58205e3d7b73eb05b1566af4258b8d59e2a96b5
Glitch (third-prison) <none>
2018-11-23 23:49:39 +0000

    🐔📡 Updated with Glitch

---
+++ public/lib/onload.js
@@ -0,0 +1,7 @@
+// This really just sets everything up when you first load the page.
+window.onload = function(){
+  create_settings();
+  create_meshes_menu();
+  load_inventory_list();
+  load_new_task(null);
+}
